be1cb6d736fd6e75b48f33756a44ccaee74adeb6
Hanna Bieri <h.bieri@mbpr-hbieri.local>
2020-11-30 12:39:06 +0100

    Test Commit

---
+++ _SE_01_Dojo/SE01_Dojo_HannaBieri.py
@@ -0,0 +1,8 @@
+def create_board():
+	board = [[n for n in range(0,7)], [n for n in range(8,14)], [n for n in range(14,21)], [n for n in range(21,28)]]
+	print(board[0])
+	print(board[1])
+	print(board[2])
+	print(board[3])
+
+create_board()
